80f2116f1e763dec0ad9d325448c22f7d8bd7d52
Michael <foolsrun@swingmonkey.com>
2014-02-14 18:52:16 -0500

    Be more discreet about enqueueing scripts and styles

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -9,7 +9,7 @@ function fontawesome_shortcodes_help_styles() {
     wp_register_script( 'fontawesome-shortcodes-help', plugins_url( 'font-awesome-shortcodes/includes/help/js/bootstrap.min.js' ), 'jquery' );
 	wp_enqueue_script( 'fontawesome-shortcodes-help' );
 }
-add_action( 'admin_enqueue_scripts', 'fontawesome_shortcodes_help_styles' );
+add_action( 'edit_form_after_editor', 'fontawesome_shortcodes_help_styles' );
 
 add_filter('the_content', 'fa_fix_shortcodes');
 
