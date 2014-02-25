d0aa427cde50075eee10894f37add416dbc1af4c
FoolsRun <michael@swingmonkey.com>
2014-02-25 09:49:09 -0500

    Changes

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -9,34 +9,32 @@ function fontawesome_shortcodes_help_styles() {
     wp_register_script( 'fontawesome-shortcodes-help', plugins_url( 'fontawesome-shortcodes/includes/help/js/bootstrap.min.js' ), 'jquery' );
 	wp_enqueue_script( 'fontawesome-shortcodes-help' );
 }
-add_action( 'admin_enqueue_scripts', 'fontawesome_shortcodes_help_styles' );
+add_action( 'edit_form_top ', 'fontawesome_shortcodes_help_styles' );
 
 add_filter('the_content', 'fa_fix_shortcodes');
 
-// Create a Media Button for the help file
-//add a button to the content editor, next to the media button
-//this button will show a popup that contains inline content
-add_action('media_buttons_context', 'add_fontawesome_button');
-
 //action to add a custom button to the content editor
-function add_fontawesome_button($context) {
-  
-  //path to my icon
-  $img = FA_SHORTCODES_URL . 'images/Fontawesome_logo.svg';
+function add_fontawesome_button() {
   
   //the id of the container I want to show in the popup
-  $popup_url = 'fontawesome-shortcodes-help-popup';
+  $popup_id = 'fontawesome-shortcodes-help-popup';
   
   //our popup's title
   $title = 'Font Awesome Shortcodes Help';
 
   //append the icon
-  $context .= "<a title='{$title}'
-    href='#TB_inline?width=640inlineId={$popup_url}&height=550' class='thickbox button add_media' style='padding-left: 0px; padding-right: 0px;' title='Font Awesome Shortcodes Help'>
-    <img src='{$img}' style='height: 20px; position: relative; top: -2px;'></a>";
-  
-  return $context;
+  printf(
+    '<a title="%1$s" href="%2$s" class="thickbox button add_media" style="padding-left: 0px; padding-right: 0px;"><span class="bs_bootstrap-logo wp-media-buttons-icon"></span></a>',
+    esc_attr( $title ),
+    esc_url( '#TB_inline?width=640&height=650&inlineId=' . $popup_id )
+    //sprintf( '<img src="%s" style="height: 20px; position: relative; top: -2px;">', esc_url( $img ) )
+  );
 }
+
+// Create a Media Button for the help file
+//add a button to the content editor, next to the media button
+//this button will show a popup that contains inline content
+add_action('media_buttons', 'add_fontawesome_button', 11);
 
 function fontawesome_shortcodes_help() {
     include('fontawesome-shortcodes-help.php');
