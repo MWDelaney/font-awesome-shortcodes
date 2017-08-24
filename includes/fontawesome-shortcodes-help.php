1319c954314f7aa58ebcf3a5ff76ba25f25513e6
Michael W. Delaney <michael.delaney@gmail.com>
2017-08-24 13:11:55 -0400

    Update fontawesome-shortcodes-help.php

--- includes/fontawesome-shortcodes-help.php
+++ includes/fontawesome-shortcodes-help.php
@@ -4,15 +4,15 @@ $html = file_get_contents(dirname(__FILE__) . '/help/README.html');
 
 <script>
     jQuery(document).ready(function() {
-         jQuery("#fa-selector .icon-lists a").click(function() {
+        jQuery("#fa-selector .icon-lists a").click(function() {
             var icon = jQuery( "i", this ).attr('class').replace('fa fa-', '');
             var sendto = "[fa type=\"" + icon + "\"]";
             var win = window.dialogArguments || opener || parent || top;
             win.send_to_editor(sendto);
             jQuery('#fontawesome-shortcodes-help').modal('hide');
             return false;
-        }); 
-        
+        });
+
         jQuery(".fa-insert-code").click(function() {
             var example = jQuery( this ).parent().prev().find("code").text();
             var lines = example.split('\n');
@@ -46,7 +46,7 @@ $html = file_get_contents(dirname(__FILE__) . '/help/README.html');
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4>Font Awesome Shortcodes Help</h4>  
+                <h4>Font Awesome Shortcodes Help</h4>
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#fa-selector" data-toggle="tab">Icon Selector</a></li>
                     <li><a href="#fa-shortcode-reference" data-toggle="tab">Shortcode Reference</a></li>
@@ -58,45 +58,29 @@ $html = file_get_contents(dirname(__FILE__) . '/help/README.html');
 
         <div id="fa-top" class="tab-content">
             <div class="tab-pane active" id="fa-selector">
-          
-            <p class="alert alert-warning" style="margin-top: 20px;">Click an icon to automatically insert it into the WordPress editor.</p>
-                        
-            <div id="icons" class="icon-lists">
-                <div class="form-group">
-                <div class="input-group input-group-lg">
-                    <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-search"></i></span>
-                    <input class="search form-control input-lg" placeholder="Search Icons" />
-                </div>
-                </div>
 
-                  <div class="row fontawesome-icon-list list">
+              <p class="alert alert-warning" style="margin-top: 20px;">Click an icon to automatically insert it into the WordPress editor.</p>
+            <div class="list-group">
+                <a class="list-group-item" href="#new">New Icons in 4.2</a>
+                <a class="list-group-item" href="#web-application">Web Application Icons</a>
+                <a class="list-group-item" href="#file-type">File Type Icons</a>
+                <a class="list-group-item" href="#spinner">Spinner Icons</a>
+                <a class="list-group-item" href="#form-control">Form Control Icons</a>
+                <a class="list-group-item" href="#payment">Payment Icons</a>
+                <a class="list-group-item" href="#chart">Chart Icons</a>
+                <a class="list-group-item" href="#currency">Currency Icons</a>
+                <a class="list-group-item" href="#text-editor">Text Editor Icons</a>
+                <a class="list-group-item" href="#directional">Directional Icons</a>
+                <a class="list-group-item" href="#video-player">Video Player Icons</a>
+                <a class="list-group-item" href="#brand">Brand Icons</a>
+                <a class="list-group-item" href="#medical">Medical Icons</a>
+            </div>
+            <div class="icon-lists">
+                <?php include(dirname(__FILE__) . '/help/icon-list.html') ?>
+            </div>
 
-                <?php 
-                    // Initialize the library with custom args.
-                    Better_Font_Awesome_Library::get_instance( $args );
+            </div>
 
-                    // Get the active Better Font Awesome Library Object.
-                    $my_bfa = Better_Font_Awesome_Library::get_instance();
-
-                    // Get info on the Better Font Awesome Library object.
-                    $version = $my_bfa->get_version();
-                    $stylesheet_url = $my_bfa->get_stylesheet_url();
-                    $prefix = $my_bfa->get_prefix();
-                    $icons = $my_bfa->get_icons();
-
-                    // Output all available icons.
-                    $i = 1;
-                    foreach ( $icons as $icon ) { ?>
-                <div class="fa-hover col-xs-6 col-sm-4"><a class="icon" href="#"><i class="fa fa-<?php echo $icon; ?>"></i> <?php echo $icon; ?></a></div>
-                    <?php
-                    }
-                ?>
-                    </div>
-
-                </div>
-                
-            </div>  
-            
             <?php
                 # Put HTML content in the document
                 $html = preg_replace('/(<a href="http:[^"]+")>/is','\\1 target="_blank">',$html);
