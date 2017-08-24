2ea7d85f1d3c9b22a4dfa56907ff1af6c09ea96c
Michael W. Delaney <michael.delaney@gmail.com>
2017-08-24 12:55:28 -0400

    Update actions-filters.php

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -9,24 +9,18 @@ add_action( 'admin_enqueue_scripts', 'font_awesome_shortcodes_styles_all' );
 
 
 function fontawesome_shortcodes_help_styles() {
-  if ( ! function_exists( 'get_current_screen' ) )
-       return;
-    $screen = get_current_screen();
-    if($screen->parent_base != "gf_edit_forms") {
-        wp_register_style( 'fa-font', plugins_url( 'font-awesome-shortcodes/includes/help/fa-font.css' ) );
-        wp_register_style( 'fontawesome-shortcodes-help', plugins_url( 'font-awesome-shortcodes/includes/help/css/fontawesome-shortcodes-help.css' ) );
-        wp_register_style( 'fontawesome-shortcodes-help-icons', plugins_url( 'font-awesome-shortcodes/includes/help/css/font-awesome.min.css' ) );
-        wp_register_style( 'bootstrap-modal', plugins_url( 'font-awesome-shortcodes/includes/help/css/bootstrap-modal.css' ) );
+	wp_register_style( 'fa-font', plugins_url( 'font-awesome-shortcodes/includes/help/fa-font.css' ) );
+	wp_register_style( 'fontawesome-shortcodes-help', plugins_url( 'font-awesome-shortcodes/includes/help/css/fontawesome-shortcodes-help.css' ) );
+	wp_register_style( 'fontawesome-shortcodes-help-icons', plugins_url( 'font-awesome-shortcodes/includes/help/css/font-awesome.min.css' ) );
+	wp_register_style( 'bootstrap-modal', plugins_url( 'font-awesome-shortcodes/includes/help/css/bootstrap-modal.css' ) );
 
-        wp_enqueue_style( 'fontawesome-shortcodes-help' );
-        wp_enqueue_style( 'fontawesome-shortcodes-help-icons' );
-        wp_enqueue_style( 'bootstrap-modal' );
-        wp_enqueue_style( 'fa-font' );
+	wp_enqueue_style( 'fontawesome-shortcodes-help' );
+	wp_enqueue_style( 'fontawesome-shortcodes-help-icons' );
+	wp_enqueue_style( 'bootstrap-modal' );
+	wp_enqueue_style( 'fa-font' );
 
-        wp_register_script( 'bootstrap', plugins_url( 'font-awesome-shortcodes/includes/help/js/bootstrap.min.js' ) );
-        wp_enqueue_script( 'bootstrap' );
-    }
-
+	wp_register_script( 'bootstrap', plugins_url( 'font-awesome-shortcodes/includes/help/js/bootstrap.min.js' ) );
+	wp_enqueue_script( 'bootstrap' );
 }
 add_action( 'media_buttons', 'fontawesome_shortcodes_help_styles' );
 
@@ -34,28 +28,23 @@ add_filter('the_content', 'fa_fix_shortcodes');
 
 //action to add a custom button to the content editor
 function add_fontawesome_button() {
-  if ( ! function_exists( 'get_current_screen' ) )
-       return;
-    $screen = get_current_screen();
-    if($screen->parent_base != "gf_edit_forms") {
 
-        //the id of the container I want to show in the popup
-        $popup_id = 'fontawesome-shortcodes-help';
+	//the id of the container I want to show in the popup
+	$popup_id = 'fontawesome-shortcodes-help';
 
-        //our popup's title
-        $title = 'Font Awesome Shortcodes Help';
+	//our popup's title
+	$title = 'Font Awesome Shortcodes Help';
 
-        //append the icon
-        //append the icon
-        printf(
-        '<a data-toggle="modal" data-target="#fontawesome-shortcodes-help" title="%2$s" href="%3$s" class="%4$s"><span class="fa_font-awesome-logo wp-media-buttons-icon"></span></a>',
-        esc_attr( $popup_id ),
-        esc_attr( $title ),
-        esc_url( '#' ),
-        esc_attr( 'button add_media font-awesome-shortcodes-button')
-        //sprintf( '<img src="%s" style="height: 20px; position: relative; top: -2px;">', esc_url( $img ) )
-        );
-    }
+	//append the icon
+	//append the icon
+	printf(
+	'<a data-toggle="modal" data-target="#fontawesome-shortcodes-help" title="%2$s" href="%3$s" class="%4$s"><span class="fa_font-awesome-logo wp-media-buttons-icon"></span></a>',
+	esc_attr( $popup_id ),
+	esc_attr( $title ),
+	esc_url( '#' ),
+	esc_attr( 'button add_media font-awesome-shortcodes-button')
+	//sprintf( '<img src="%s" style="height: 20px; position: relative; top: -2px;">', esc_url( $img ) )
+	);
 }
 
 // Create a Media Button for the help file
